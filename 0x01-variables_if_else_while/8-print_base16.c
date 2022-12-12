76e309d54648a1737fc3a86e2acdfea09a07c213
Jerry cham <zuleejojo@gmail.com>
2022-12-12 15:44:29 -0800

    add

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int num = '0'; /*Initialize num to 0*/
 char letter = 'a'; /*Initialize letter to a*/
-while (num <= '9') /*Print 0-9*/ 
+while (num <= '9') /*Print 0-9*/
 {
 putchar(num);
 num++;
@@ -18,7 +18,7 @@ while (letter <= 'f') /*Print a-f*/
 {
 putchar(letter);
 letter++;
-} 
+}
 putchar('\n');
 return (0);
 }
