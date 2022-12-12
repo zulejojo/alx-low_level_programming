a680057e9de63fc4da5365dbb90c0fe5b0b8b4ff
Jerry cham <zuleejojo@gmail.com>
2022-12-12 15:11:28 -0800

    add

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - return single integer from 0-10
- *
- * Return: Always 0 (Success)
+* main - return single integer from 0-10
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - return single integer from 0-10
- *
- * Return: Always 0 (Success)
+* main - return single integer from 0-10
+*
+* Return: Always 0 (Success)
 */
 int main(void)
- 
 {
 int num = '0';
 while (num <= '9')
