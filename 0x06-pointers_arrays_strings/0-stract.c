cdd059ce6b3e7e8acc66aa86a7191438b72c208c
Jerry cham <zuleejojo@gmail.com>
2022-12-27 17:30:48 -0800

    more pointers

--- 0x06-pointers_arrays_strings/0-stract.c
+++ 0x06-pointers_arrays_strings/0-stract.c
@@ -13,11 +13,11 @@ length = 0;
 while (dest[length] != '\0')
 {
 length++;
-}
+ }
 for (j = 0; src[j] != '\0'; j++, length++)
 {
 dest[length] = src[j];
-} 
+}
 dest[length] = '\0';
 return (dest);
 }
