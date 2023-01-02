7ebe9d0799eb7772d458cdd03d96623626a1b2f9
Jerry cham <zuleejojo@gmail.com>
2023-01-02 07:42:53 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
 *of the memory area pointed to by s with the constant byte b
 *Returns a pointer to the memory area s
 */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int counter;
 for (counter=0; counter<n; counter++)
