f720269c0409fd907e47192ad83792746255425a
Jerry cham <zuleejojo@gmail.com>
2023-01-02 09:07:29 -0800

    memset

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int size=n;
+int size = n;
 if (size > 0)
 {
-int counter;
-for (counter = 0; counter < size; counter++)
-dest[counter]=src[counter];
+int i;
+for (i = 0; i < size; i++)
+dest[i] = src[i];
 }
 return (dest);
 }
