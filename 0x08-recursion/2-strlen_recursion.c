1f7eab719266e1e6e842b9c0278192f04b2d9d74
Jerry cham <zuleejojo@gmail.com>
2023-01-05 17:19:11 -0800

    recurison

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
 * _strlen_recursion:- returns the length of string
 * @s: variable to return its length
-* Retun: length of string in interger
+* Return: length of string in interger
 */
 int _strlen_recursion(char *s)
 {
