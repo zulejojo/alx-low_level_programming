4616c13bf6268d52e84d5094cef6d9b7f1695153
Jerry cham <zuleejojo@gmail.com>
2023-01-05 17:11:58 -0800

    recurison

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen_recursion:- returns the length of string
+* @s: variable to return its length
+* Retun: length of string in interger
+*/
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1+_strlen_recursion(s + 1)
+	  return (0);
+}
+}
