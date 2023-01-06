51e43b126a5b9307b33277719940d5eb0a993563
Jerry cham <zuleejojo@gmail.com>
2023-01-05 16:40:38 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,15 @@
 /**
 * A function that prints a string , followed by a new line
 * the program uses _puts_recurson
-
 * returns nothings
 */
-void _puts_recursion(char *s)
+void _puts_recursion(char *s) /* delcaring function puts recursion */
 {
-if  (*s=='\0')
+if  (*s == '\0')
 {
 _putchar('\n');
 return;
-}  
+}
 _putchar(*s);
 _puts_recursion(s + 1);
 }
