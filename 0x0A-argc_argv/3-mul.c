8879b2c68294249d4be3cb94bde29ae83b254e94
Jerry cham <zuleejojo@gmail.com>
2023-01-14 15:10:26 -0800

    whatsmyname

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
 *
 * Return: 0 on success, 1 on error
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int num1, num2, result;
 if (argc != 3)
@@ -19,6 +19,6 @@ return (1);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 result = num1 * num2;
-printf("%d\n", result);
+printf("%i\n", result);
 return (0);
 }
