16483d30e56ce35b1716f0f09953020cb90742b3
Jerry cham <zuleejojo@gmail.com>
2023-01-14 14:52:02 -0800

    whatsmyname

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
 *
 * Return: 0 on success
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-printf("%d\n", argc - 1);
+(void)argv; /*ignore argv*/
+printf("%i\n", argc - 1);
 return (0);
 }
