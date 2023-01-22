98cabfaa18537b4c8544031b0c5389b2ce735291
Jerry cham <zuleejojo@gmail.com>
2023-01-22 12:45:52 -0800

    the woman

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,137 @@
-#include <stdlib.h>
+#include "main.h"
 
-#include <string.h>
+/**
+
+ *_strlen - count array
+
+ *@s: array of elements
+
+ *Return: 1
+
+ */
 
 
 
-char *_strdup(char *str) {
+int _strlen(char *s)
   
-  if (str == NULL) {
+{
+  
+  unsigned int i;
+  
+
+  
+  i = 0;
+  
+  while (s[i] != '\0') /*count character of string*/
     
-    return NULL;
-    
-  }
+    {
+      
+      i++;
+      
+    }
   
-  int len = strlen(str);
+
   
-  char *dup = malloc(len + 1);
-  
-  if (dup == NULL) {
-    
-    return NULL;
-    
-  }
-  
-  strcpy(dup, str);
-  
-  return dup;
+  return (i);
   
 }
 
+
+
+/**
+
+ *_strcpy - copy arrays
+
+ *@src: array of elements
+
+ *@dest: dest array
+
+ *Return: dest
+
+ */
+
+
+
+char *_strcpy(char *dest, char *src)
+  
+{
+  
+  int i = 0;
+  
+
+  
+  while (src[i] != '\0')
+    
+    {
+      
+      dest[i] = src[i];
+      
+      i++;
+      
+    }
+  
+  dest[i] = '\0';
+  
+
+  
+  return (dest);
+  
+}
+
+
+
+/**
+
+ *_strdup - array for prints a string
+
+ *@str: array of elements
+
+ *Return: pointer
+
+ */
+
+
+
+char *_strdup(char *str)
+  
+{
+  
+  char *dst;
+  
+  unsigned int size;
+  
+
+  
+  if (str == 0)
+    
+    {
+      
+      return (NULL);
+      
+    }
+  
+
+  
+  size = _strlen(str) + 1;
+  
+
+  
+  dst = (char *) malloc(size * sizeof(char));
+  
+
+  
+  if (dst == 0)
+    
+    {
+      
+      return (NULL);
+      
+    }
+  
+
+  
+  _strcpy(dst, str);
+  
+  return (dst);
+  
+}
