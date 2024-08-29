fbdd025a8184e362670d3c42edb88ee23cfdd80a
zulejojo <zulejojo@gmail.com>
2024-08-28 17:53:41 -0700

    dynamic lib

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - checks if its a digit
+* @c: paramter
+* Return: 0 or 1
+*/
+int  _isdigit(int c)
+{
+if (c >= 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
